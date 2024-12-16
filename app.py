80be78d901f6324bea34a1aafc7e7d4fa9fa532d
salman91ahmed <salman.ahmed.a35@gmail.com>
2024-12-16 21:25:08 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+# operator ifelse statement
+#assignment operator
+num1: int=12
+num2: int=2
+#arithmetice operator
+sum:int= num1 + num2
+sub:int=num1 - num2
+multi:int= num1 * num2
+modulous:int= num1 % num2
+expo:int= num1 ** num2 
+print(sum)
+print(sub)
+print(multi)
+print(modulous)
+print(20//3)
+print(15%3)
+print(expo)
